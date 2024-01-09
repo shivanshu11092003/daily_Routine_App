d8a808cf3781683fc1e9319337c6ec0250f2483c
shivanshu11092003 <148114391+shivanshu11092003@users.noreply.github.com>
2024-01-09 19:54:15 +0530

    some updates1.1

--- app/src/main/java/com/example/practiceapp/dailytrack.kt
+++ app/src/main/java/com/example/practiceapp/dailytrack.kt
@@ -2,22 +2,37 @@ package com.example.practiceapp
 
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 import android.widget.ProgressBar
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintSet.Layout
+import com.example.practiceapp.databinding.ActivityDailytrackBinding
 
 class dailytrack : AppCompatActivity() {
+    private lateinit var binding: ActivityDailytrackBinding
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         var progressBar: ProgressBar? = null
-
-
-
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dailytrack)
+        binding= ActivityDailytrackBinding.inflate(layoutInflater)
+
+
+
+
+
+
+        setContentView(binding.root)
+
+        val clickbtn=findViewById<RelativeLayout>(R.id.progress_layout)
+        clickbtn.setOnClickListener {
+            intent=Intent(this,calorieStatus::class.java)
+            startActivity(intent)
+        }
 
         progressBar = findViewById(R.id.progress_bar)
         var progressBar2: ProgressBar = findViewById(R.id.progress_bar2)
